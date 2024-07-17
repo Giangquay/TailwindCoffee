1b47942e4052a36a7ce7485f8230c3471e2b1f80
Giang Minh <giangnm@ikong.vn>
2024-07-17 10:45:24 +0700

    commit header

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'light-coffee': "#c89f94"
+      }
+    },
+  },
+  plugins: [],
+}
+
